5345613e1a31bb98729d8b809aaaa1a10cf43c56
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-17 12:13:45 -0300

    3 - Definindo condições de vitória

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ let playing = false;
 //Events
 
 document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick)
+});
 
 //Functions
+
+function itemClick(event){
+
+    let item = event.target.getAttribute('data-item');
+
+    if(square[item] === ""){
+
+        square[item] = player;
+
+        renderSquare();
+        togglePlayer();
+    }
+}
 
 function reset(){
     warning = '';
@@ -47,5 +63,62 @@ function renderSquare(){
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+
+    checkGame();
+
+}
+
+function togglePlayer(){
+    
+    player = (player === 'x') ? 'o' : 'x';
+
+    renderInfo();
+}
+
+function checkGame(){
+    if(checkGameWinnerFor('x')){
+        warning = "O 'x' venceu";
+        playing = false;
+    }else if(checkGameWinnerFor('o')){
+        warning = "O 'o' venceu";
+        playing = false;
+    }else if(isFull()){
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkGameWinnerFor(player){
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos){
+        let pArray = pos[w].split(',')
+        pArray.every(option => square[option] === player);
+    }
+
+    /*
+        O 'for' acima funciona da mesma que esse aqui abaixo
+        
+        pArray.every((option) => {
+
+            if(square[option] === player){
+                return true;
+            }else{
+                return false;
+            }
+        })
+    */
+}
+
+function isFull(){
 
 }
